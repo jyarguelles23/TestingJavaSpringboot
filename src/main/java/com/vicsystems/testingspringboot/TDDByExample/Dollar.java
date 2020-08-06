854f3b30db62d87a1c2da43c6043fb9d67e0457f
Admin <jyarguelles23@gmail.com>
2020-08-06 12:15:42 -0400

    Currency implementation

--- src/main/java/com/vicsystems/testingspringboot/TDDByExample/Dollar.java
+++ src/main/java/com/vicsystems/testingspringboot/TDDByExample/Dollar.java
@@ -2,14 +2,15 @@ package com.vicsystems.testingspringboot.TDDByExample;
 
 public class Dollar extends Money {
 
-
-    public Dollar(int amount) {
-        this.amount = amount;
+    public Dollar(int amount,String currency) {
+        super(amount,currency);
     }
 
    public Money times( int multiplier) {
-        return new Dollar(amount * multiplier);
+        return Money.dollar(amount * multiplier);
     }
+
+
 
 
 }
--- src/main/java/com/vicsystems/testingspringboot/TDDByExample/Money.java
+++ src/main/java/com/vicsystems/testingspringboot/TDDByExample/Money.java
@@ -1,16 +1,30 @@
 package com.vicsystems.testingspringboot.TDDByExample;
 
+import java.util.Map;
+
 public abstract class Money {
 
     protected int amount;
+    protected String currency;
 
+    public Money(int amount,String currency) {
+        this.amount = amount;
+        this.currency=currency;
+    }
     public abstract Money times(int amount);
-    public static Dollar dollar(int amount){
-        return new Dollar(amount);
+
+   public String currency(){
+        return currency;
     }
-    public static Franc franc(int amount){
-        return new Franc(amount);
+
+    public static Money dollar(int amount){
+        return new Dollar(amount,"USD");
     }
+
+    public static Money franc(int amount){
+        return new Franc(amount,"CHF");
+    }
+
     public boolean equals(Object object) {
         Money money = (Money) object;
         //NO es lo mismo 5 francos que 5 dolares y en el equals la prueba no pasa porque les dice que son iguales para evitar eso se hace el sgte codigo
