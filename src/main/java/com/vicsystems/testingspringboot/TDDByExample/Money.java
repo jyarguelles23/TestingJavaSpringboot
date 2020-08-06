ec183893b21bc8bd78a80fc7cefcdd2a3d4f7aaf
Admin <jyarguelles23@gmail.com>
2020-08-06 11:51:10 -0400

    Factory Method Design Pattern Tested

--- src/main/java/com/vicsystems/testingspringboot/TDDByExample/Money.java
+++ src/main/java/com/vicsystems/testingspringboot/TDDByExample/Money.java
@@ -1,9 +1,16 @@
 package com.vicsystems.testingspringboot.TDDByExample;
 
-public class Money {
+public abstract class Money {
 
     protected int amount;
 
+    public abstract Money times(int amount);
+    public static Dollar dollar(int amount){
+        return new Dollar(amount);
+    }
+    public static Franc franc(int amount){
+        return new Franc(amount);
+    }
     public boolean equals(Object object) {
         Money money = (Money) object;
         //NO es lo mismo 5 francos que 5 dolares y en el equals la prueba no pasa porque les dice que son iguales para evitar eso se hace el sgte codigo
