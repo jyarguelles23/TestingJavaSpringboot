acb6cbe98dbfd5041fefcb4cb8111c632d8cf5b9
Admin <jyarguelles23@gmail.com>
2020-08-07 12:34:56 -0400

    Creating a funcion addrate para agregar las tasas de cambio en el banco, agregamos el objeto banco a la operacion reduce en la interfaz Expression y asi cada vez que vayas a hacer un reduce ver si es la misma moneda o es distinta y te hace la conversion.

--- src/main/java/com/vicsystems/testingspringboot/TDDByExample/Bank.java
+++ src/main/java/com/vicsystems/testingspringboot/TDDByExample/Bank.java
@@ -1,8 +1,19 @@
 package com.vicsystems.testingspringboot.TDDByExample;
 
+import java.util.HashMap;
+
 public class Bank   {
 
+    HashMap<Pair,Integer> rateMap= new HashMap<>();
     public Money reduce(Expression source, String toCurrency) {
-        return source.reduce(toCurrency);
+        return source.reduce(this,toCurrency);
+    }
+
+    public int rate(String from,String to){
+        return (from.equals(to)) ? 1 : rateMap.get(new Pair(from,to));
+    }
+
+    public void addRate(String from, String to, int rate) {
+        rateMap.put(new Pair(from,to),rate);
     }
 }
