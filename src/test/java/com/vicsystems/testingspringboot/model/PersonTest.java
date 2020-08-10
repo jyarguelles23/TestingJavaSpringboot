6d17c69c279ae71289c31e36a897c89cbb78b978
Admin <jyarguelles23@gmail.com>
2020-08-10 12:29:11 -0400

    JUnit Grouped assertions

---
+++ src/test/java/com/vicsystems/testingspringboot/model/PersonTest.java
@@ -0,0 +1,28 @@
+package com.vicsystems.testingspringboot.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+    @Test
+    void groupedAssertions(){
+        //given
+        Person person =new Person(1l,"Jesus","Arguelles");
+
+        //then
+        assertAll("Test Props Set",
+                () -> assertEquals(person.getFirstName(),"Jesus"),
+                () -> assertEquals(person.getLastName(),"Arguelles"));
+    }
+
+    @Test
+    void groupedAssertionsMSG(){
+        //given
+        Person person =new Person(1l,"Jesus","Arguelles");
+        //then
+        assertAll("Test Props Set",
+                () -> assertEquals(person.getFirstName(),"Jesus", "First name failed"),
+                () -> assertEquals(person.getLastName(),"Arguelles",()-> "Last name failed"));
+    }
+}
