614a8950f8a862b26ee7c416143257d6e1cc2326
Admin <jyarguelles23@gmail.com>
2020-08-14 11:31:59 -0400

    JUnit Test Dependency INjection

--- src/test/java/com/vicsystems/testingspringboot/Model/PersonTest.java
+++ src/test/java/com/vicsystems/testingspringboot/Model/PersonTest.java
@@ -1,7 +1,7 @@
 package com.vicsystems.testingspringboot.Model;
 
 import com.vicsystems.testingspringboot.ModelTests;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,5 +25,18 @@ class PersonTest implements ModelTests {
         assertAll("Test Props Set",
                 () -> assertEquals(person.getFirstName(),"Jesus", "First name failed"),
                 () -> assertEquals(person.getLastName(),"Arguelles",()-> "Last name failed"));
+    }
+
+    @RepeatedTest(value= 10,name="{displayName} : {currentRepetition} - {totalRepitions")
+    @DisplayName("RepeatedTest")
+    @Test
+    void myRepeatedTest(){
+        System.out.println("repitiendo");
+    }
+
+    @RepeatedTest(5)
+    void myRepeatedTestWithDI(TestInfo testInfo, RepetitionInfo repetitionInfo) {
+        System.out.println(testInfo.getDisplayName() + ": " + repetitionInfo.getCurrentRepetition());
+
     }
 }
