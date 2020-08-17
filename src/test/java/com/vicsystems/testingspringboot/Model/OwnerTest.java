5f43f9203c37eb6f489172c1a959761a11f980a3
Kbron87 <jyarguelles23@gmail.com>
2020-08-17 12:21:04 -0400

    CSV File Source Parameterized Test JUnit Advanced JUnit Testing

--- src/test/java/com/vicsystems/testingspringboot/Model/OwnerTest.java
+++ src/test/java/com/vicsystems/testingspringboot/Model/OwnerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -49,7 +50,7 @@ class OwnerTest implements ModelTests {
         System.out.println(ownerType);
     }
 
-    @DisplayName("Enum Source Test")
+    @DisplayName("CSV Source Test")
     @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
     @CsvSource({
             "FL,1,1",
@@ -58,6 +59,13 @@ class OwnerTest implements ModelTests {
             "MI,1,1"
     })
     void csvInputTest(String stateName,int val1,int val2){
+        System.out.println(stateName+" : "+ val1+ " : "+ val2);
+    }
+
+    @DisplayName("CSV File Source Test")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @CsvFileSource(resources = "/input.csv",numLinesToSkip = 1)
+    void csvFromFileTest(String stateName,int val1,int val2){
         System.out.println(stateName+" : "+ val1+ " : "+ val2);
     }
 }
