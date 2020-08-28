c549fb8083c3ae8a7daae845bfa6e993b9dc7652
Kbron87 <jyarguelles23@gmail.com>
2020-08-28 14:05:17 -0400

    Verification Process Mockito BDD

--- src/test/java/com/vicsystems/testingspringboot/Mockito/InjectingMocks/SpecialitySDJpaServiceTest.java
+++ src/test/java/com/vicsystems/testingspringboot/Mockito/InjectingMocks/SpecialitySDJpaServiceTest.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -49,15 +50,18 @@ class SpecialitySDJpaServiceTest {
     //BDD MOckito
     @Test
     void findByIdBddTest() {
+        //Given
         Speciality speciality = new Speciality();
 
         given(specialtyRepository.findById(1L)).willReturn(Optional.of(speciality));
-
+         //When
         Speciality foundSpecialty = service.findById(1L);
 
+        //Then
         assertThat(foundSpecialty).isNotNull();
-
-        verify(specialtyRepository).findById(anyLong());
+        then(specialtyRepository).should().findById(anyLong());
+       // then(specialtyRepository).should(times(1)).findById(anyLong());
+        then(specialtyRepository).shouldHaveNoMoreInteractions();
     }
 
     @Test
